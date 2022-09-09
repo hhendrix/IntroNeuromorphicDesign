a0605177ac8ea4b1968d28e792b215fb541fb3db
Harry Lopez <harry.lopez@overactive.com>
2022-09-09 09:24:25 -0500

    Initial Commit

---
+++ IntroNeuromorphicDesign/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IntroNeuromorphicDesign
+//
+//  Created by Harry Lopez on 9/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IntroNeuromorphicDesign/IntroNeuromorphicDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroNeuromorphicDesignApp.swift
+//  IntroNeuromorphicDesign
+//
+//  Created by Harry Lopez on 9/09/22.
+//
+
+import SwiftUI
+
+@main
+struct IntroNeuromorphicDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
